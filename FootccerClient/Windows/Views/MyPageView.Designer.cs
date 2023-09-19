c92502150b0e8b0ca873c3e4f8cee872114ba011
dkdlxmofficial <dkdlxmofficial@gmail.com>
2023-09-19 11:12:36 +0900

    +1 + MyPage 브랜치 생성

--- FootccerClient/Windows/Views/MyPageView.Designer.cs
+++ FootccerClient/Windows/Views/MyPageView.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
@@ -40,11 +41,21 @@
             this.label1.TabIndex = 1;
             this.label1.Text = "마이페이지";
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "마이페이지";
+            // 
             // MyPageView
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1249, 719);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "MyPageView";
             this.Text = "MyPageView";
@@ -56,5 +67,6 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
